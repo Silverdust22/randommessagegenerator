e7a54e75ee949617997678c1ea58568d42639ca6
Pink Sky <swipat@gmail.com>
2024-10-16 10:08:30 +0100

    messagegen commit

--- main.js
+++ main.js
@@ -1,20 +1,8 @@
- 
-
-const generaterandomword = wordlength =>{
+ const generaterandomword = wordlength =>{
     return Math.floor(Math.random() * wordlength)
 }
 
-function printArray(element){
-    if (element === 'Today you will receive a box of chocolates.'){
-    console.log(element,String.fromCodePoint(0x1F36C));
-}else if(element === 'Today you will receive a lovely bouquet of flowers.'){
-    console.log(element,String.fromCodePoint(0x1F395));
-}
-    else{
-    console.log(element);
-    }
-}
-
+function operatebutton(){
 const phrases = {
 day:['great','good','fantastic','wonderful'],
 items:['box of chocolates','lovely bouquet of flowers','set of your favourite books','message from your old pal'],
@@ -30,12 +18,12 @@ for (let word in phrases){
     }else if(word === 'items'){
         newArray.push(`Today you will receive a ${phrases[word][randomResult]}.`);
     }else if(word === 'places'){
-        newArray.push(`Soon you will visit ${phrases[word][randomResult]}!`);
+        newArray.push(`You will visit ${phrases[word][randomResult]} soon.`);
     }else{
         console.log('Please try again')
     } 
 }
-    //console.log(newArray);
-    newArray.forEach(printArray);
-        
-//console.log(String.fromCodePoint(0x1F36C));
+   document.getElementById("hello").innerHTML = newArray.join("<br>");
+}
+  
+ 
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Random Message Generator
+This project consists of following files:
+1. index.html
+2. styles.css
+3. main.js
+4. images.jpg
+
+This code renders three Random messages. JavaScript uses arrays in an object. Methods like floor(),random(), join() are used to create messages. To bring interactivity between HTML elements and JavaScript objects, getElementById() method and innerHTML property is applied.
