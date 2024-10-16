28d7647537ded840300be4a52f751105e436e580
Pink Sky <swipat@gmail.com>
2024-10-16 15:46:23 +0100

    javascript changes

--- main.js
+++ main.js
@@ -4,26 +4,24 @@
 
 function operatebutton(){
 const phrases = {
-day:['great','good','fantastic','wonderful'],
 items:['box of chocolates','lovely bouquet of flowers','set of your favourite books','message from your old pal'],
-places:['London','Paris','New York','Los Angeles']
+places:['London','Paris','New York','Los Angeles'],
+day:['great','good','fantastic','wonderful']
 }
 
 const newArray = [];
 for (let word in phrases){
     randomResult = generaterandomword(phrases[word].length);
     //console.log(randomResult);
-    if(word === 'day'){
-        newArray.push(`Have a ${phrases[word][randomResult]} day!`);
-    }else if(word === 'items'){
+    if(word === 'items'){
         newArray.push(`Today you will receive a ${phrases[word][randomResult]}.`);
     }else if(word === 'places'){
         newArray.push(`You will visit ${phrases[word][randomResult]} soon.`);
-    }else{
-        console.log('Please try again')
-    } 
+    }else if(word === 'day'){
+        newArray.push(`Have a ${phrases[word][randomResult]} day!`);
+    }    
 }
-   document.getElementById("hello").innerHTML = newArray.join("<br>");
+  document.getElementById("hello").innerHTML = newArray.join("<br>");
 }
   
  
